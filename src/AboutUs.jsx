e3f9c1a10898c9d9c4c6697ff2a39d4a187c21a2
rbui-dev <richardmatthewbui@gmail.com>
2024-12-14 15:53:12 -0500

    forgot to include other files

---
+++ src/AboutUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './AboutUs.css';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <p className="about-us-content">
+      At Paradise Nursery, we bring nature closer with a wide selection of high-quality plants to beautify your space and promote a sustainable lifestyle. From air-purifying to aromatic varieties, we have something for every plant lover.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
