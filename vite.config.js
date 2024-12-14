061e2f974e3aa15c45618432d86c54f95f24e695
rbui-dev <richardmatthewbui@gmail.com>
2024-12-14 16:31:12 -0500

    fix href

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/paradise-nursery-shopping-cart-app",
+    base: "/react-coursera-course",
     plugins: [react()],
-    build: {
-        assetsInclude: ['**/*.{css,js,jsx,jpg,jpeg,png,svg}'] // Include common asset types
-      }
 })
